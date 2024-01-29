600a90a1e3038a802e5d9a7d96be66a278b8d6bc
Mohsin Khan <mohsink9.3ss@gmail.com>
2024-01-30 01:48:09 +0500

    added middleware and me route and helper function to get token

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,49 @@
 "use client"
 
+import axios from "axios"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
 export default function ProfilePage() {
+    const router = useRouter();
+    const [user, setUser] = useState("nothing")
+    const logout = async () => {
+        try {
+            const response = await axios.get("/api/users/logout");
+            console.log('res logout', response);
+            toast.success('Logout Successfully')
+            router.push('/login')
+        } catch (error: any) {
+            console.log('err', error.message);
+            toast.error(error.message)
+
+        }
+    };
+
+    const getUserDetails = async () => {
+        try {
+            const response = await axios.get("/api/users/me");
+            console.log('res me', response.data);
+            setUser(response.data.data._id);
+        } catch (error: any) {
+            console.log('err', error.message);
+            toast.error(error.message)
+
+        }
+    }
+
+    useEffect(() => {
+        getUserDetails();
+    }, [])
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
+            <h2 className="p-1 rounded bg-green-500">{user === 'nothing' ? "Nothing" : <Link href={`/profile/${user}`}>{user}</Link>}</h2>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded" onClick={logout}>Logout</button>
         </div>
     )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse, NextRequest } from "next/server";
+
+// This function can be marked async if using await inside
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+
+    const isPublicPath = path === '/login' || path === '/signup';
+
+    const token = request.cookies.get("token")?.value || '';
+
+    // check if user is already loggedin then redirect it to home page
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
+    // check if user is not loggedin then redirect it to login page
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+}
+
+// See matching paths below to learn more
+
+export const config = {
+    matcher: [
+        '/',
+        '/profile/:path*',
+        '/login',
+        '/signup'
+    ]
+}
